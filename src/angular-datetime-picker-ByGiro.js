95d6649f1de7126aa37167202213d70bf6d08539
bygiro <girotomaselli@gmail.com>
2016-08-31 15:52:21 +0200

    release 0.0.3

--- src/angular-datetime-picker-ByGiro.js
+++ src/angular-datetime-picker-ByGiro.js
@@ -1,39 +1,29 @@
-/*! angular-datetime-picker-ByGiro - v0.0.2 - 18 april 2015
+/*! angular-datetime-picker-ByGiro - v0.0.3 - 18 april 2015
 * Copyright (c) G. Tomaselli <girotomaselli@gmail.com> 2015; Licensed  
 */
 
 angular.module('ByGiro.datetimePicker', [])
-	.directive('bgDatetimePicker', [ function () { // Angular Directive Declaration
-		return {
-			restrict: 'A',                 // The Directive is with Attribute 			
-			scope: {
-				dataVal: "=?ngModel"
-			},
-			link: function (scope, element, attributes) {
-				var bg = (typeof jQuery != 'undefined') ? jQuery : angular.element,
-				options = {},res;
-				if(attributes.bgDtpOptions){
-					options = scope.$parent[attributes.bgDtpOptions];
-				}
-				
-				bg(element).val(scope.dataVal);
+.directive('bgDatetimePicker', [function () {
+	
+	contrFunction = ['$scope', '$timeout', '$element', '$attrs', function($scope, $timeout, $element, $attrs){
 
-				res = bg(element).datetimepickerByGiro(options);
-				
-				// update model on datepicker change
-				bg(element).on('datetimepickerByGiro_changed', function(eve, data){
-					var value = data.getValue(true);
-					
-					// update the scope
-					var phase = scope.$root.$$phase;
-					if (phase == '$apply' || phase == '$digest') {
-						scope.dataVal = value;
-					} else {
-						scope.$apply(function(){
-							scope.dataVal = value;
-						});
-					}
-				});
-			}
-		}
-    }]);
+			$element.datetimepickerByGiro($scope.options)
+			.on('datetimepickerByGiro_changed', function(eve, data){
+
+				if($attrs.ngModel){
+					$scope.ngModel = data.getValue(true);
+				}
+
+				$timeout();
+			});
+		}];
+	
+	return({
+		scope: {
+			options: "=bgDtpOptions",
+			ngModel: "=?"
+		},
+		restrict: "A",
+		controller: contrFunction
+	});
+}]);
